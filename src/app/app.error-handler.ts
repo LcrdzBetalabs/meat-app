fe28b4028a5b3218ba9cf0840cea1ea69dbbc07c
LcrdzBetalabs <51802641+LcrdzBetalabs@users.noreply.github.com>
2019-07-12 17:15:04 -0300

    Att 12/07

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,11 +1,11 @@
-import {Response} from '@angular/http'
+import {HttpErrorResponse} from '@angular/common/http'
 import {Observable} from 'rxjs/Observable'
 import 'rxjs/add/observable/throw'
 
 export class ErrorHandler {
   static handleError(error: Response | any){
     let errorMessage: string
-    if(error instanceof Response){
+    if(error instanceof HttpErrorResponse){
       errorMessage = `Erro ${error.status} ao obter URL ${error.url} - ${error.statusText}`
     }else{
       errorMessage = error.toString()
--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -6,6 +6,13 @@ import {Restaurant} from './restaurant/restaurant.model'
 import {RestaurantsService} from './restaurants.service'
 
 import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/from'
+import {Observable} from 'rxjs/Observable'
+
 
 @Component({
   selector: 'mt-restaurants',
@@ -46,8 +53,12 @@ export class RestaurantsComponent implements OnInit {
     })
 
     this.searchControl.valueChanges
+        .debounceTime(500)
+        .distinctUntilChanged()
         .switchMap(searchTerm =>
-        this.restaurantsService.restaurants(searchTerm))
+        this.restaurantsService
+        .restaurants(searchTerm)
+        .catch(error => Observable.from([])))
         .subscribe(restaurants => this.restaurants = restaurants)
 
     this.restaurantsService.restaurants()
