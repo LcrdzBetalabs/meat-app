1d6c666317d776055cdc636d82af539f0e100783
LcrdzBetalabs <51802641+LcrdzBetalabs@users.noreply.github.com>
2019-07-15 18:16:00 -0300

    Att 15/07

---
+++ backend/users.ts
@@ -0,0 +1,16 @@
+export class  User {
+  constructor(public email: string,
+              public name: string,
+              private password: string){}
+
+  matches(another: User): boolean {
+    return another !== undefined &&
+           another.email === this.email &&
+           another.password === this.password
+  }
+}
+
+export const users: {[key: string]: User} = {
+  "luiz@betalabs.com": new User('luiz@betalabs.com', 'Luiz', 'betalabs'),
+  "teste@betalabs.com": new User('teste@betalabs.com', 'Teste', 'betalabs2'),
+}
